8d06f7b79ab2c8fc5beb252fc8cda0ef20e2b164
dkj0 <dakolke@outlook.com>
2018-02-16 12:31:17 -0800

    Update CLI-ARM-deployment.ps1

--- 200 ARM Deploy/CLI-ARM-deployment.ps1
+++ 200 ARM Deploy/CLI-ARM-deployment.ps1
@@ -1,13 +1,13 @@
-  # download an ARM template
-	# Edit paramaters 
-	# change directory to location of template 
-	
-	# use to validate a template for deployment
-	az group deployment validate --resource-group wordpress --template-file C:\users\dakolke\desktop\template-CLJ-10\template.json --parameters C:\Users\dakolke\Desktop\template-CLJ-10\parameters.json
-	
-	# use to actually deploy 
-	az group deployment create --resource-group wordpress --template-file C:\users\dakolke\desktop\template-CLJ-10\template.json --parameters C:\Users\dakolke\Desktop\template-CLJ-10\parameters.json
-	
-	Details and more options:
-	https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-template-deploy-cli
+# download an ARM template
+# Edit paramaters 
+# change directory to location of template 
+
+# use to validate a template for deployment
+az group deployment validate --resource-group wordpress --template-file C:\users\dakolke\desktop\template-CLJ-10\template.json --parameters C:\Users\dakolke\Desktop\template-CLJ-10\parameters.json
+
+# use to actually deploy 
+az group deployment create --resource-group wordpress --template-file C:\users\dakolke\desktop\template-CLJ-10\template.json --parameters C:\Users\dakolke\Desktop\template-CLJ-10\parameters.json
+
+# Details and more options:
+# https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-group-template-deploy-cli
 
